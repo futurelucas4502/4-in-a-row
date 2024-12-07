6229a3b87ad912b58082221a2dc1b377da437916
Lucas Wilson <maytheforcebewithulww@gmail.com>
2024-12-07 02:49:07 +0000

    Game complete
    The code is very messy and could do with a lot of work but this is technically fully functional

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <SFML/Window.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Preprocess function types
 void userClick(int xPos, int yPos);
@@ -11,6 +13,8 @@ bool clickRectangle(sfVector2f clickPos);
 void label(const char *userText, int x, int y, int fontSize, sfColor textColour);
 void button(sfRectangleShape *rectangle, int rectangleWidth, int rectangleHeight, int rectangleX, int rectangleY, sfColor rectangleColour, const char *userText, int fontSize, sfColor textColor);
 void winCheck();
+void shapeFill();
+void gameOver();
 
 // This is the actual Render window its defined like this as it is a pointer to an sfRenderWindow that we will then interact with
 sfRenderWindow *window;
@@ -27,6 +31,10 @@ sfText *text;
 // Create title font and text
 sfFont *titleFont;
 sfText *title;
+char *startButtonText;
+char *infoLabelText;
+
+// TODO: Maybe add some kind of try catch system so if it errors anywhere we clear all memory before closing as this probably has memory leaks in its current state
 
 int main(int argc, char const *argv[]) {
     // Main setup of window and event handling
@@ -51,6 +59,7 @@ int main(int argc, char const *argv[]) {
     if (!titleFont) {
         puts("Unable to create title font, aborting.");
         sfRenderWindow_destroy(window);
+        free(startButtonText);
         return 1;
     }
     title = sfText_create();
@@ -66,6 +75,7 @@ int main(int argc, char const *argv[]) {
         sfRenderWindow_destroy(window);
         sfText_destroy(title);
         sfFont_destroy(titleFont);
+        free(startButtonText);
         return 1;
     }
     text = sfText_create();
@@ -73,18 +83,17 @@ int main(int argc, char const *argv[]) {
 
     // Start of interactable button initialisations
 
+    startButtonText = malloc(strlen("Start Game") + 1);
+    strcpy(startButtonText, "Start Game");
     startButton = sfRectangleShape_create();
     quitButton = sfRectangleShape_create();
 
+    infoLabelText = malloc(strlen("Player 1s Turn") + 1);
+    strcpy(infoLabelText, "Player 1s Turn");
+
     // Start filling the circle array with circle objects which will then be used to create the board
 
-    for (size_t i = 0; i < 7; i++) {
-        for (size_t j = 0; j < 6; j++) {
-            circle[i][j] = sfCircleShape_create();
-            sfCircleShape_setRadius(circle[i][j], 30.0);
-            sfCircleShape_setFillColor(circle[i][j], sfWhite);
-        }
-    }
+    shapeFill();
 
     // Finish filling circle array
 
@@ -135,10 +144,10 @@ int main(int argc, char const *argv[]) {
 
         sfRenderWindow_drawText(window, title, NULL);  // Title rendering
         // label("O", -1, -6, 25);                  // absolute top left
-        label("Player 1 is red!", 875, 150, 25, sfWhite);  // we use 875 as it is the width of the button below divided by 2 add the x position of the rectangle which gives us the exact coordinates for the centre of the rectangle which means this label is in line with the label of the button below making things look neat
+        label(infoLabelText, 875, 150, 25, sfWhite);  // we use 875 as it is the width of the button below divided by 2 add the x position of the rectangle which gives us the exact coordinates for the centre of the rectangle which means this label is in line with the label of the button below making things look neat
 
-        button(startButton, 250, 50, 750, 200, sfBlue, "Start button", 25, sfWhite);
-        button(quitButton, 250, 50, 750, 650, sfBlue, "Quit button", 25, sfWhite);
+        button(startButton, 250, 50, 750, 200, sfBlue, startButtonText, 25, sfWhite);
+        button(quitButton, 250, 50, 750, 650, sfBlue, "Quit Game", 25, sfWhite);
 
         // End rendering the information card on the right
 
@@ -162,6 +171,8 @@ int main(int argc, char const *argv[]) {
     // sfSprite_destroy(sprite);
     // sfTexture_destroy(texture);
     sfRenderWindow_destroy(window);
+    free(startButtonText);
+    free(infoLabelText);
     return 0;
 }
 
@@ -191,6 +202,16 @@ void label(const char *userText, int x, int y, int fontSize, sfColor textColour)
     sfFloatRect textSize = sfText_getGlobalBounds(text);
     sfText_setPosition(text, (sfVector2f){x - textSize.width / 2, y});  // we do this to centre the text around the points given making it much easier to work out where to place labels
     sfRenderWindow_drawText(window, text, NULL);
+}
+
+void shapeFill() {
+    for (size_t i = 0; i < 7; i++) {
+        for (size_t j = 0; j < 6; j++) {
+            circle[i][j] = sfCircleShape_create();
+            sfCircleShape_setRadius(circle[i][j], 30.0);
+            sfCircleShape_setFillColor(circle[i][j], sfWhite);
+        }
+    }
 }
 
 void userClick(int xPos, int yPos) {
@@ -226,12 +247,16 @@ bool clickCircle(sfVector2f clickPos) {  // im not sure if this is the best appr
                             if (belowColour.b != 255) {                                                   // as long as blue is not 255 it means we have either a yellow or red circle below
                                 sfCircleShape_setFillColor(circle[i][j], user1sTurn ? sfRed : sfYellow);  // fill the circle the user clicked with red or yellow depending on the turn
                                 user1sTurn = !user1sTurn;
+                                infoLabelText = malloc(strlen(user1sTurn ? "Player 1s Turn" : "Player 2s Turn") + 1);
+                                strcpy(infoLabelText, user1sTurn ? "Player 1s Turn" : "Player 2s Turn");
                             } else {
                                 puts("There is no colour below this");
                             }
                         } else {  // if we're on the bottom row we can just set the colour as we know this is a valid move
                             sfCircleShape_setFillColor(circle[i][j], user1sTurn ? sfRed : sfYellow);
                             user1sTurn = !user1sTurn;
+                            infoLabelText = malloc(strlen(user1sTurn ? "Player 1s Turn" : "Player 2s Turn") + 1);
+                            strcpy(infoLabelText, user1sTurn ? "Player 1s Turn" : "Player 2s Turn");
                         }
 
                     } else {  // the current circle colour is not white
@@ -250,6 +275,9 @@ bool clickRectangle(sfVector2f clickPos) {
     sfFloatRect quitButtonPos = sfRectangleShape_getGlobalBounds(quitButton);
     if (sfFloatRect_contains(&startButtonPos, clickPos.x, clickPos.y)) {
         gameRunning = true;
+        shapeFill();
+        startButtonText = malloc(strlen("Game Running") + 1);
+        strcpy(startButtonText, "Game Running");
         return true;
     } else if (sfFloatRect_contains(&quitButtonPos, clickPos.x, clickPos.y)) {
         puts("Closing render window.");
@@ -260,12 +288,49 @@ bool clickRectangle(sfVector2f clickPos) {
 }
 
 void winCheck() {
-    // for (size_t i = 0; i < 42; i++) {                                  // loop all 42 circles allowing our checks
-    //     sfColor circleColour = sfCircleShape_getFillColor(circle[i]);  // get the current circle colour
-    //     // TODO: check where we are on the board and determine the directions we could have win in then after checking which directions we could win in we create arrays of each of the pieces in each direction
-    //     // TODO: change the circle array to be A 2D MATRIX as this will be much easier to check against
+    int playerColour = sfColor_toInteger(user1sTurn ? sfYellow : sfRed);  // this is set to the opposite as when a player finishes their move the user turn is updated but in this case we want to ignore that update
 
-    //     if (i + 7 > 35) {  // if were on the bottom row we can only check above and diagonal
-    //     }
-    // }
+    // Horizontal Win Check
+    for (int i = 6; i > 0; i--) {      // height
+        for (int j = 7; j > 0; j--) {  // width
+            if (sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j + 1][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j + 2][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j + 3][i])) == playerColour) {
+                gameOver();
+            }
+        }
+    }
+
+    // Vertical Win Check
+    for (int i = 6; i > 0; i--) {      // height
+        for (int j = 7; j > 0; j--) {  // width
+            if (sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i + 1])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i + 2])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i + 3])) == playerColour) {
+                gameOver();
+            }
+        }
+    }
+
+    // Diagonal Win Check left to right
+    for (int i = 6; i > 0; i--) {      // height
+        for (int j = 7; j > 0; j--) {  // width
+            if (sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 1][i + 1])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 2][i + 2])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 3][i + 3])) == playerColour) {
+                gameOver();
+            }
+        }
+    }
+
+    // Diagonal Win Check right to left
+    for (int i = 6; i > 0; i--) {      // height
+        for (int j = 7; j > 0; j--) {  // width
+            if (sfColor_toInteger(sfCircleShape_getFillColor(circle[j][i])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 1][i - 1])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 2][i - 2])) == playerColour && sfColor_toInteger(sfCircleShape_getFillColor(circle[j - 3][i - 3])) == playerColour) {
+                gameOver();
+            }
+        }
+    }
+}
+
+void gameOver() {
+    infoLabelText = malloc(strlen(!user1sTurn ? "Player 1 wins!" : "Player 2 wins!") + 1);
+    strcpy(infoLabelText, !user1sTurn ? "Player 1 wins!" : "Player 2 wins!");
+    startButtonText = malloc(strlen("Play Again?") + 1);
+    strcpy(startButtonText, "Play Again?");
+    gameRunning = false;
 }
